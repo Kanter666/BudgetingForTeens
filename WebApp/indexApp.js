de48cda6983ef80d308cf5eb5c0daef17a0af07c
Kanter666 <ak2g15@soton.ac.uk>
2017-04-01 21:43:34 +0100

    Added jquery, solved some errors

--- WebApp/indexApp.js
+++ WebApp/indexApp.js
@@ -1,10 +1,33 @@
+var main = document.querySelector('#main'),
+  addCircleBtn = document.querySelector('#add-circle'),
+  container = main.querySelector('.container'),
+  circles = container.querySelectorAll('.circle'),
+  mainConfig = {
+    autoRotate: true,
+    autoAddCircle: true,
+    interval: null,
+    turn: 'circle'
+  }
 
+ function setPosition() {
+  circles = main.querySelectorAll('.circle');
+
+  var circleSize = circles[0].getBoundingClientRect().width;
+
+  for (var i = 0; i < circles.length; i++) {
+
+    transformX = window.height()/2
+    transformY = window.width()/2
+    
+    circles[i].style.transform = "translate3d(" + transformX + ", " + transformY + ", 0)";
+    circles[i].style.opacity = 1;
+  };
+}
 function registerWindow(){
-	document.getElementById("demo").innerHTML = "Hello World";
 	window.open ("register.html");
 	return false;
 }
 
 function login(){
-
+	document.getElementById("botton").innerHTML = "Hello World";
 }
